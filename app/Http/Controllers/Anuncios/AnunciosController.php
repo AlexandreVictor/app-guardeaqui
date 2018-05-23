dcb31d342f22289297f5ee0a7b2ed63da4c92a13
alexandrevictor <alexandrevct@hotmail.com>
2018-05-23 19:47:49 -0300

    consulta no anuncio controller

--- app/Http/Controllers/Anuncios/AnunciosController.php
+++ app/Http/Controllers/Anuncios/AnunciosController.php
@@ -11,12 +11,12 @@ class AnunciosController extends Controller
         public function index(){
         
         //VERIFICAR POIS ESTA DANDO ERRO
-        //$Dados = DB::table('anuncios')
-        //->where('pago','=','1')
-        //->where('aprovado','=','1')
-        //->where('status','=','1')
-        //->select('nome', 'endereco')->get();
-        //return $Dados;
+        $Dados = DB::table('anuncios')
+        ->where('pago','=','1')
+        ->where('aprovado','=','1')
+        ->where('status','=','1')
+        ->select('nome', 'endereco')->get();
+        return $Dados;
 
         $Anuncio = anuncio::with('anuncio')->get();
         return response()->json($Anuncio);
